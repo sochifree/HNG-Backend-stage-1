4e15a6d7abe180db161eb04adcab30ebff864f0c
sochifree <111816813+sochifree@users.noreply.github.com>
2025-10-28 04:25:50 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,22 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 
 const stringRoute = require('./routes/strings')
-const { errorHandler, notFoundHandler } = require('./utils/error');
+const errorHandler = require('./utils/error');
 
 
 const app = express();
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.json({ limit: '1mb' }));
 
 app.use('/strings', stringRoute)
 
-app.get('/', (req, res)=> res.json({ status: 'ok' }));
+app.get('/health', (req, res)=> res.json({ status: 'ok' }));
 
-// 404
-app.use(notFoundHandler);
-
-// central error handler
 app.use(errorHandler);
+
 
 module.exports = app
