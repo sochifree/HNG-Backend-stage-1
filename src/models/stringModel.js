25a4efe5357568c457a1f4c234161a1c64d91e51
sochifree <111816813+sochifree@users.noreply.github.com>
2025-10-29 04:28:44 +0100

    Update stringModel.js

--- src/models/stringModel.js
+++ src/models/stringModel.js
@@ -1,21 +1,11 @@
 const mongoose = require('mongoose')
 const stringSchema = new mongoose.Schema({
-    id: { type: String, require: true, unique: true },
+    id: { type: String, required: true, unique: true },
     value: { type: String, required: true },
-     properties: {type: Object, required: true
-    //     length: Number,
-    //     is_palindrome: Boolean,
-    //     unique_characters: Number,
-    //     word_count: Number,
-    //     sha_256_hash: String,
-    //     character_frequency_map: {type: Map, of: Number }
-    },
+     properties: {type: Object, required: true },
     created_at: { type: Date, default: ()=> new Date().toISOString()}
 });
 
 module.exports = mongoose.model('StringRecord', stringSchema);
 
 
-//value must exist and be a string. Else
-//respond 422 (unprocessable entity) if value exist but not a string          
-//or 400 (bad request) if body missing value
